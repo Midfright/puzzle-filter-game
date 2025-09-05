3fbaf286218a2375c17b4a0c3ec16446a476751a
Midfright <john.muji@gmail.com>
2025-09-05 19:12:07 +0800

    Update comments

--- utils/game_logic.py
+++ utils/game_logic.py
@@ -18,8 +18,5 @@ def validate_solution(cur_pos, orig_pos):
     pass
 
 def get_stage_config(stage_num):
-    # Returns: {'grid_size': 3, 'filter_intensity': 'low', 'filters_to_use': [...]}
-    # Stage 1: 3x3, light filters
-    # Stage 2: 4x4, medium filters
-    # Stage 3+: 5x5, heavy filters
+    # stage, filters, puzzle slicing(3x3, 4x4)
     pass
