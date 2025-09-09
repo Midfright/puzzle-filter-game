10a3c93e70cc517033088875264dbe8218b7f436
sb33b <jsheffron@gmail.com>
2025-09-09 16:49:59 +0800

    implement generate_puzzle, validate_solution

--- utils/game_logic.py
+++ utils/game_logic.py
@@ -2,21 +2,43 @@
 game_logic.py
 '''
 
+import utils.image_processor as ip
+
 def get_grid_size(stage_num):
-    # returns integer
-    # really only need this if we want different grid size/puzzle slice number
-    pass
+    return stage_num + 1
 
 def generate_puzzle(stage_num):
     # use image_processor functions like fetch, slide, apply filters
     # return dictionary with puzzle data like stage, grid size, pieces, original order
-    pass
+
+    filter_list = ip.get_filter_list()
+    filter_to_apply = filter_list[stage_num % len(filter_list)]
+
+    image = ip.fetch_image(stage_num)
+    pieces = ip.slice_image(image, stage_num)
+
+    for piece in pieces:
+        piece['image_data'] = ip.apply_filter(piece['image_data'], filter_to_apply)
+        piece['filter'] = filter_to_apply
+    
+    shuffled_pieces = ip.shuffle_pieces(pieces)
+    shuffled_pieces = ip.piece_to_base64(shuffled_pieces)
+
+    return {
+        'stage': stage_num,
+        'grid_size': get_grid_size(stage_num),
+        'shuffled_pieces': shuffled_pieces,
+        'correct_order': [piece['id'] for piece in pieces],
+    }
 
 def validate_solution(cur_pos, orig_pos):
-    # get two lists of piece positions and compare
-    # return boolean to confirm validity
-    pass
 
-def get_stage_config(stage_num):
-    # stage, filters, puzzle slicing(3x3, 4x4)
-    pass
+    # accepts ordered list of piece positions represented by piece id
+    # returns boolean to confirm validity
+
+    if not isinstance(cur_pos, list) or not isinstance(orig_pos, list):
+        raise ValueError("cur_pos and orig_pos must be lists")
+    if all(isinstance(pos, int) for pos in cur_pos) and all(isinstance(pos, int) for pos in orig_pos):
+        raise ValueError("cur_pos and orig_pos must be lists of integers")
+
+    return cur_pos == orig_pos
